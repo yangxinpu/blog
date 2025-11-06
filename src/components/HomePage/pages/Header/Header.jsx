c9ba8d36bc7ee96cba4fa04362eaad3d1d6ba69e
Yang <1813481502@qq.com>
2025-11-06 13:51:35 +0800

    docs: 更新文档，格式化了代码

--- src/components/HomePage/pages/Header/Header.jsx
+++ src/components/HomePage/pages/Header/Header.jsx
@@ -17,12 +17,12 @@ const generateColorShades = baseColor => {
     const hexToRgb = hex => {
         const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
         return result
-        ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16),
-            }
-        : null;
+            ? {
+                  r: parseInt(result[1], 16),
+                  g: parseInt(result[2], 16),
+                  b: parseInt(result[3], 16),
+              }
+            : null;
     };
 
     // 将RGB转换为十六进制
