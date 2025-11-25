57dc4d320cfcfbeef203ac113e3b2dbfcf0cf926
Yang <1813481502@qq.com>
2025-11-25 20:59:08 +0800

    build：将修改打包到docs目录中

--- src/components/TechPage/pages/LeftItem/LeftItem.jsx
+++
@@ -1,9 +0,0 @@
-import './style.scss';
-
-export default function LeftItem() {
-    return (
-        <div id="TechPage-Left-item">
-            <h2>LeftItem</h2>
-        </div>
-    );
-}
--- src/components/TechPage/pages/RightItem/RightItem.jsx
+++
@@ -1,9 +0,0 @@
-import './style.scss';
-
-export default function RightItem() {
-    return (
-        <div id="TechPage-Right-item">
-            <h2>RightItem</h2>
-        </div>
-    );
-}
