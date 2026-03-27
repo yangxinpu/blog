e8bb4fcfda1d9a662dbeffba0d78b87db7eb51af
Yang <1813481502@qq.com>
2026-03-27 20:42:17 +0800

    refactor: 将元数据从单独文件移至App组件内
    将personMeta和footerData从单独的meta.ts文件移动到App.tsx组件中，以简化项目结构并减少文件数量

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,76 @@ import Thoughts from './pages/Thoughts/Thoughts';
 import TextAnimation from './pages/TextAnimation/TextAnimation';
 import AuroraRisePage from './pages/AuroraRisePage/AuroraRisePage';
 import NeonSprintPage from './pages/NeonSprintPage/NeonSprintPage';
-import { personMeta, footerData } from './content/meta';
 import { Message, Dropdown } from './components';
 import styles from './App.module.scss';
 import logoImage from './assets/Images/logo.png';
+
+const personMeta = {
+  nickname: 'NaiLuo',
+  capitalNickname: 'NAILUO',
+  githubLink: 'https://github.com/Nailuo',
+  email: 'nailuo@example.com'
+};
+
+const footerData = {
+  techStack: [
+    {
+      name: 'React',
+      link: 'https://react.dev/',
+      icon: 'https://cdn.simpleicons.org/react/61DAFB'
+    },
+    {
+      name: 'TypeScript',
+      link: 'https://www.typescriptlang.org/',
+      icon: 'https://cdn.simpleicons.org/typescript/3178C6'
+    },
+    {
+      name: 'Next.js',
+      link: 'https://nextjs.org/',
+      icon: 'https://cdn.simpleicons.org/nextdotjs/00D5C4'
+    },
+    {
+      name: 'Tailwind CSS',
+      link: 'https://tailwindcss.com/',
+      icon: 'https://cdn.simpleicons.org/tailwindcss/06B6D4'
+    },
+    {
+      name: 'JavaScript',
+      link: 'https://developer.mozilla.org/docs/Web/JavaScript',
+      icon: 'https://cdn.simpleicons.org/javascript/F7DF1E'
+    },
+    {
+      name: 'Vue',
+      link: 'https://vuejs.org/',
+      icon: 'https://cdn.simpleicons.org/vuedotjs/4FC08D'
+    },
+    {
+      name: 'Vite',
+      link: 'https://vite.dev/',
+      icon: 'https://cdn.simpleicons.org/vite/646CFF'
+    },
+    {
+      name: 'Webpack',
+      link: 'https://webpack.js.org/',
+      icon: 'https://cdn.simpleicons.org/webpack/8DD6F9'
+    },
+    {
+      name: 'Sass',
+      link: 'https://sass-lang.com/',
+      icon: 'https://cdn.simpleicons.org/sass/CC6699'
+    },
+    {
+      name: 'Node.js',
+      link: 'https://nodejs.org/',
+      icon: 'https://cdn.simpleicons.org/nodedotjs/5FA04E'
+    },
+    {
+      name: 'Bun',
+      link: 'https://bun.sh/',
+      icon: 'https://bun.sh/logo.svg'
+    }
+  ]
+};
 
 function App() {
   const { t, i18n } = useTranslation();
